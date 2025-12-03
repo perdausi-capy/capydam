5fe290856d95a12f4ca2f1d59148accc0b576ab3
perdausi <perdausi.hairul@capytech.com>
2025-12-03 14:54:42 +0800

    another bug fix

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import client from '../api/client';
 import { useAuth } from '../context/AuthContext';
 import logoIcon from '../assets/capytech-fav.png'
