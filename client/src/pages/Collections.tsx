229f75d9116be366b2ee1f1888625c22f77adc84
perdausi <perdausi.hairul@capytech.com>
2026-01-07 17:08:54 +0800

    Optimization

--- client/src/pages/Collections.tsx
+++ client/src/pages/Collections.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import client from '../api/client';
 import { 
