41a425f8a62b3bff2edae631fb676f0e50e74076
perdausi hairul <perdausi.hairul@capytech.com>
2026-06-19 13:14:48 +0800

    updated Analytics Page

--- client/src/pages/AdminAnalytics.tsx
+++ client/src/pages/AdminAnalytics.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import client from '../api/client';
